0e851082f0ae14925b5f0e3e9cfb70655f371bc0
이신일 <itsinil@gmail.com>
2018-10-23 20:10:45 +0900

    symjs 변경작업

--- src/symverse-tx.js
+++ src/symverse-tx.js
@@ -1,8 +1,10 @@
 var helper = require('./symverse-helper');
+var SymverseCitizenTx = require('./symverse-citizen-tx');
 var EthereumTx = require('ethereumjs-tx');
 var utils = require('web3-utils');
 
 var SymverseTx = function () {
+
     function sign (params, pt) {
         params.value = helper.toWei(params.value);
         if (!params.hasOwnProperty('nonce')) {
@@ -18,8 +20,21 @@ var SymverseTx = function () {
         let serializedTx = tx.serialize();
         return '0x' + serializedTx.toString('hex');
     }
+
+    function citizenSign (params, pt){
+        for (let i in params) {
+            params[i] = utils.toHex(params[i]);
+        }
+        const tx = new SymverseCitizenTx(params);
+        const p = Buffer.from(pt, 'hex');
+        tx.sign(p);
+        let serializedTx = tx.serialize();
+        return '0x' + serializedTx.toString('hex');
+    }
+
     return {
-        sign: sign
+        sign,
+        citizenSign
     };
 };
 
--- tests/symverse.spec.js
+++ tests/symverse.spec.js
@@ -3,14 +3,33 @@
  */
 const should = require('should');
 const symjs =  require ('../index.js');
+var ethers = require('ethers');
 
 describe('테스트', function () {
 
     it('should return the statusCode 200', () => {
-        symjs.Symverse.version.should.be.equal('0.1');
+        symjs.Symverse.version.should.be.equal('0.0.0-beta3');
     });
 
     it('should return user array', () => {
-        symjs.Symverse.tx
+
+        const pt = "1be4e194a09ae22094d3f1affe41020be64b0e38595145623ecbfcdd6dc259f9";
+        var params = {
+            "from":"0x00000000000123456789",
+            "to":"0x00000000000123456789",
+            "nonce":"0x00",
+            "symid":"0x00000000000123456789",
+            "pubkeyhash":"0xd360ef98ec6bd17623ab09d8310c9b62883076a1",
+            "country":"0x1",
+            "status":"0x1",
+            "credit":"0x1",
+            "role":"0x2",
+            "org":"0x1",
+            "writetime":"2018-10-20T17:13:40Z"
+        };
+        let rlpData = symjs.Symverse.tx.citizenSign(params, pt);
+        console.log(rlpData);
+        console.log(ethers.utils.RLP.decode(rlpData))
+
     });
 });
