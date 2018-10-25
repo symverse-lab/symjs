0500aaa5693f0e4ebd7db8c58de9b2a46e64f26e
이신일 <itsinil@gmail.com>
2018-10-25 18:53:38 +0900

    symverse 와 ether 분리

--- src/symverse-tx.js
+++ src/symverse-tx.js
@@ -1,6 +1,6 @@
 var helper = require('./symverse-helper');
 var SymverseCitizenTx = require('./symverse-citizen-tx');
-var EthereumTx = require('ethereumjs-tx');
+var EthereumTx = require('./symverse-transaction-tx');
 var utils = require('web3-utils');
 
 var SymverseTx = function () {
@@ -8,12 +8,11 @@ var SymverseTx = function () {
     function sign (params, pt) {
         params.value = helper.toWei(params.value);
         if (!params.hasOwnProperty('nonce')) {
-            params.nonce = '0x00';
+            params.nonce = '0x0';
         }
         for (let i in params) {
             params[i] = utils.toHex(params[i]);
         }
-        // nonce: '0x00'
         const tx = new EthereumTx(params);
         const p = Buffer.from(pt, 'hex');
         tx.sign(p);
--- tests/symverse.spec.js
+++ tests/symverse.spec.js
@@ -11,12 +11,32 @@ describe('라이브러리 기능 체크', function () {
         symjs.Symverse.version.should.be.equal('0.0.0-beta3');
     });
 
+    it('일반 트랜젝션', () => {
+        const pt = "1be4e194a09ae22094d3f1affe41020be64b0e38595145623ecbfcdd6dc259f9";
+        var params = {
+            "from":"0x00000000000123456789",  // "0" 10개 + 본인의 SymId (20자리)
+            "nonce":"0x0",
+            "gasPrice": 1,
+            "gasLimit": 21000,
+            "to":"0x00000000000123456789",
+            "value": 1,
+            "data": "0x0",
+            "chainId": -1
+            //v
+            //r
+            //s
+        };
+        let rlpData = symjs.Symverse.tx.sign(params, pt);
+        console.log(rlpData);
+        console.log(ethers.utils.RLP.decode(rlpData))
+    });
+
     it('시티즌 트랜젝션', () => {
         const pt = "1be4e194a09ae22094d3f1affe41020be64b0e38595145623ecbfcdd6dc259f9";
         var params = {
             "from":"0x00000000000123456789",  // "0" 10개 + 본인의 SymId (20자리)
             "to":"0x00000000000123456789", // // 받을 노드 ( 20자리 )
-            "nonce":"0x0",
+            "nonce":"0x1",
             "symid":"0x00000000000123456789", // "0" 10개 + 본인의 SymId (20자리)
             "pubkeyhash":"0xd360ef98ec6bd17623ab09d8310c9b62883076a1", // 본인의 address
             "country":"0x1",  //국가
@@ -24,7 +44,7 @@ describe('라이브러리 기능 체크', function () {
             "credit": "0x0", //신용
             "role":"0x2", //계정 룰 ( 1: CA 인증용 / 2. 일반 거래용 )
             "org":"0x1", //dapp 기관
-            "writetime": 1540342995 //요청 시간
+            "writetime": 1540342998 //요청 시간
             //v
             //r
             //s
@@ -32,6 +52,5 @@ describe('라이브러리 기능 체크', function () {
         let rlpData = symjs.Symverse.tx.citizenSign(params, pt);
         console.log(rlpData);
         console.log(ethers.utils.RLP.decode(rlpData))
-
     });
 });
